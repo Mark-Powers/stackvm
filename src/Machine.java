a2e3cde6837d0f88f7df900d6ec96d4f9584e193
Mark Powers <markppowers0@gmail.com>
2018-09-25 21:48:48 -0400

    Initial commit

---
+++ src/Machine.java
@@ -0,0 +1,67 @@
+import java.util.HashMap;
+
+public class Machine {
+	private int[] s;
+	private int sp;
+	private int pointer;
+	private HashMap<String, Integer> table;
+	private HashMap<Integer, String> pointers;
+	private HashMap<String, Integer> labels;
+	
+	public Machine(int size) {
+		s = new int[size];
+		sp = -1;
+		pointer = 0;
+		table = new HashMap<String, Integer>();
+		pointers = new HashMap<Integer, String>();
+		labels = new HashMap<String, Integer>();
+	}
+	
+	public int pop() {
+		return s[sp--];
+	}
+	
+	public void push(int v) {
+		s[++sp] = v;
+	}
+	
+	public void copy() {
+		s[sp+1] = s[sp];
+		sp++;
+	}
+	
+	public void rvalue(String id) {
+		push(table.get(id));
+	}
+	
+	public void lvalue(String id) {
+		push(pointer);
+		pointers.put(pointer++, id);
+	}
+	
+	public String getLValue(int p) {
+		return pointers.get(p);
+	}
+	
+	public void assign(int p, int val) {
+		table.put(getLValue(p), val);
+	}
+	
+	public void print() {
+		System.out.print("{");
+		for(int i = 0; i <= sp; i++)
+		{
+			System.out.print(s[i] + " " );
+		}
+		System.out.println("}");
+	}
+
+	public void label(String l, int index) {
+		labels.put(l, index); 
+	}
+	
+	public int getLabel(String l) {
+		return labels.get(l); 
+	}
+}
+
---
+++ src/VM.java
@@ -0,0 +1,95 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.util.ArrayList;
+
+public class VM {
+	static int pc;
+	static String line;
+	public static void main(String args[]) {
+		if (args.length < 1) {
+			System.out.println("Must pass in file name!");
+			System.exit(1);
+		}
+		boolean debug = false;
+		if(args.length == 2) {
+			debug = true;
+		}
+		
+		File source = new File(args[0]);
+		try {
+			BufferedReader br = new BufferedReader(new FileReader(source));
+
+			Machine stack = new Machine(100);
+
+			ArrayList<String> lines = new ArrayList<String>();
+			String currline = br.readLine();
+			int index = 0;
+			while (currline != null) {
+				lines.add(currline);
+				if (currline.startsWith("label")) {
+					// -1 since the PC is loaded then incremented
+					stack.label(currline.split(" ")[1], index - 1);
+				}
+				index++;
+				currline = br.readLine();
+			}
+			br.close();
+
+			for ( pc = 0; pc < lines.size(); pc++) {
+				line = lines.get(pc);
+				if (line.startsWith("halt")) {
+					break;
+				} else if (line.startsWith("print")) {
+					System.out.println(stack.pop());
+				} else if (line.startsWith("rvalue")) {
+					stack.rvalue(line.split(" ")[1]);
+				} else if (line.startsWith("lvalue")) {
+					stack.lvalue(line.split(" ")[1]);
+				} else if (line.startsWith("push")) {
+					stack.push(Integer.parseInt(line.split(" ")[1]));
+				} else if (line.startsWith("pop")) {
+					stack.pop();
+				} else if (line.startsWith(":=")) {
+					int value = stack.pop();
+					stack.assign(stack.pop(), value);
+				} else if (line.startsWith("+")) {
+					stack.push(stack.pop() + stack.pop());
+				} else if (line.startsWith("-")) {
+					int tmp = stack.pop();
+					stack.push(stack.pop() - tmp);
+				} else if (line.startsWith("/")) {
+					int tmp = stack.pop();
+					stack.push(stack.pop() / tmp);
+				} else if (line.startsWith("%")) {
+					int tmp = stack.pop();
+					stack.push(stack.pop() % tmp);
+				} else if (line.startsWith("*")) {
+					stack.push(stack.pop() * stack.pop());
+				} else if (line.startsWith("label")) {
+					continue;
+				} else if (line.startsWith("goto")) {
+					pc = stack.getLabel(line.split(" ")[1]);
+				} else if (line.startsWith("copy")) {
+					stack.copy();
+				} else if (line.startsWith("gofalse")) {
+					if (stack.pop() == 0) {
+						pc = stack.getLabel(line.split(" ")[1]);
+					}
+				} else if (line.startsWith("gotrue")) {
+					if (stack.pop() != 0) {
+						pc = stack.getLabel(line.split(" ")[1]);
+					}
+				}
+				if (debug) {
+					System.out.println(line);
+					stack.print();
+				}
+			}
+		} catch (Exception e) {
+			System.out.println("Error: PC="+pc);
+			System.out.println(line);
+			e.printStackTrace();
+		}
+	}
+}
